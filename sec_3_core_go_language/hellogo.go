6f6b0ac9b7191ce3defb8b93abe878537edafde9
Syful Sharif <sharif.syful@gmail.com>
2023-02-16 22:04:57 +0600

    First Commit

---
+++ sec_3_core_go_language/hellogo.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	f "fmt"
+)
+
+// This is a shorthand for fmt.PrintLn
+var pl = f.Println
+
+/*
+The following one is a go function
+this is a multiline comment in go
+*/
+
+func main() {
+	pl("Hello Go!")
+	pl("this is a go print line output")
+}
